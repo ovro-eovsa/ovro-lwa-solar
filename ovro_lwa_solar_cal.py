26a71083ba8ed9047e16d09e3426f6c86da2b850
Surajit Mondal <surajit4444mondal@gmail.com>
2023-04-23 12:11:10 -0400

    added function and source component list (obtained from Gasperin et al. 2021)

--- ovro_lwa_solar_cal.py
+++ ovro_lwa_solar_cal.py
@@ -5,16 +5,22 @@
 #                              but earlier versions should be fine);
 # 		  			           Also restructured it as part of the solar OVRO-LWA calibration package for future updates
 
-from casatools import table, measures, componentlist
+from casatasks import clearcal, ft, bandpass, applycal, flagdata, tclean, uvsub
+from casatools import table, measures, componentlist,msmetadata
 import math
 import sys,os
 import numpy as np
-from casatasks import flagdata
+from astropy.coordinates import SkyCoord
+import astropy.units as u
+from astropy.wcs import WCS
+from astropy.io import fits
 import glob
+
 
 tb = table()
 me = measures()
 cl = componentlist()
+msmd=msmetadata()
 
 
 def flux80_47(flux_hi, sp, ref_freq=80., output_freq=47.):
@@ -38,10 +44,25 @@ def conv_deg(dec):
     deg = float(dd) + float(mm) / 60 + float(ss) / 3600
     return '%fdeg' % deg
 
+def get_sun_pos(solar_visibility,str_output=True):
+    tb.open(solar_visibility)
+    t0 = tb.getcell('TIME', 0)
+    tb.close()
+    ovro = me.observatory('OVRO_MMA')
+    time = me.epoch('UTC', '%fs' % t0)
+    me.doframe(ovro)
+    me.doframe(time)
+    d0 = me.direction('SUN')
+    d0_j2000 = me.measure(d0, 'J2000')
+    if str_output==True:
+        d0_j2000_str = 'J2000 %frad %frad' % (d0_j2000['m0']['value'], d0_j2000['m1']['value'])
+        return d0_j2000_str
+    return d0_j2000
 
-def gen_model_ms(visibility, ref_freq=80.0, output_freq=47.0,
-                 includesun=True, solar_flux=16000, solar_alpha=2.2,
-                 outputcl=None, verbose=True):
+
+def gen_model_cl(visibility, ref_freq=80.0, output_freq=47.0,
+                 includesun=False, solar_flux=16000, solar_alpha=2.2,
+                 modelcl=None, verbose=True, overwrite=True):
     """
     :param visibility: input visibility
     :param ref_freq: reference frequency of the preset flux values of bright sources
@@ -58,7 +79,7 @@ def gen_model_ms(visibility, ref_freq=80.0, output_freq=47.0,
             {'label': 'VirA', 'flux': '2400', 'alpha': -0.86,
              'position': 'J2000 12h30m49.42338s +12d23m28.0439s'}]
     if includesun:
-        srcs.append({'label': 'Sun', 'flux': string(solar_flux), 'alpha': solar_alpha,
+        srcs.append({'label': 'Sun', 'flux': str(solar_flux), 'alpha': solar_alpha,
                      'position': 'SUN'})
 
     tb.open(visibility)
@@ -69,6 +90,10 @@ def gen_model_ms(visibility, ref_freq=80.0, output_freq=47.0,
     time = me.epoch('UTC', '%fs' % t0)
     me.doframe(ovro)
     me.doframe(time)
+    msmd.open(visibility)
+    chan_freqs = msmd.chanfreqs(0)  
+    msmd.done()
+    avg_freq=0.5*(chan_freqs[0]+chan_freqs[-1])*1e-6
 
     for s in range(len(srcs) - 1, -1, -1):
         coord = srcs[s]['position'].split()
@@ -87,15 +112,15 @@ def gen_model_ms(visibility, ref_freq=80.0, output_freq=47.0,
         if elev < 0:
             del srcs[s]
         else:
-            scale = math.sin(elev) ** 1.6
-            print('scale {0:.1f}'.format(scale))
+            scale = math.sin(elev) ** 1.6*output_freq/avg_freq
+            print('scale {0:.2f}'.format(scale))
             srcs[s]['flux'] = flux80_47(float(srcs[s]['flux']), srcs[s]['alpha'],
                                         ref_freq=ref_freq, output_freq=output_freq) * scale
 
     cl.done()
 
-    if not outputcl:
-        outputcl = visibility.replace('.ms', '.cl')
+    if not modelcl:
+        modelcl = visibility.replace('.ms', '.cl')
     for s in srcs:
         cl.addcomponent(flux=s['flux'], dir=s['position'], index=s['alpha'],
                         spectrumtype='spectral index', freq='{0:f}MHz'.format(output_freq), label=s['label'])
@@ -103,10 +128,121 @@ def gen_model_ms(visibility, ref_freq=80.0, output_freq=47.0,
             print(
                 "cl.addcomponent(flux=%s, dir='%s', index=%s, spectrumtype='spectral index', freq='47MHz', label='%s')" % (
                     s['flux'], s['position'], s['alpha'], s['label']))
-
-    cl.rename(outputcl)
+    if os.path.exists(modelcl) and overwrite:
+        os.system('rm -rf ' + modelcl)
+    cl.rename(modelcl)
     cl.done()
-    return outputcl
+    return modelcl
+
+
+def write_source_file(file_handle,source_name,primary_beam,source_num):  #### works only if logarithimicSI is false
+    
+    try:
+        calfilepath='/home/surajit/ovro-lwa-solar/defaults/'
+        f1=open(calfilepath+source_name+".txt","r")
+        j=0
+        
+        while True:
+            line=f1.readline()
+            if not line:
+                break
+            if source_num==0 and j==0:
+                file_handle.write(line)
+            elif j!=0:
+                try:
+                    splitted=line.split(',')
+                    I_flux=float(splitted[4])
+                 
+                    beam_corrected_I_flux=I_flux*primary_beam
+                    splitted[4]=str(beam_corrected_I_flux)
+                    
+                    for k,phrase in enumerate(splitted[5:]):
+                        if k==0:
+                            splitted[5+k]='['+str(float(phrase[1:])*primary_beam)
+                        else:
+                            if phrase[-1]==']':
+                                splitted[5+k]=str(float(phrase[:-1])*primary_beam)+']'
+                                break
+                            else:
+                                splitted[5+k]=str(float(phrase)*primary_beam)
+                    line1=','.join(splitted)
+                    if splitted[5+k+1]=='false':
+                        file_handle.write(line1)
+                    else:
+                        raise RuntimeError("Function now works only if logarithmicSI is false")
+                except IndexError:
+                    pass
+                
+            j+=1
+    finally:
+        f1.close()
+    
+def gen_model_ms(visibility, ref_freq=80.0, output_freq=47.0,
+                 includesun=True, solar_flux=16000, solar_alpha=2.2,
+                 outputcl=None, verbose=True,filename='calibrator_source_list.txt'):
+    """
+    :param visibility: input visibility
+    :param ref_freq: reference frequency of the preset flux values of bright sources
+    :param output_freq: output frequency to be written into the CASA component list
+    :param includesun: if True, add a precribed solar flux to the source list
+    :return:
+    """
+    
+    srcs = [{'label': 'CasA', 'flux': '16530', 'alpha': -0.72,
+             'position': 'J2000 23h23m24s +58d48m54s'},
+            {'label': 'CygA', 'flux': '16300', 'alpha': -0.58,
+             'position': 'J2000 19h59m28.35663s +40d44m02.0970s'},
+            {'label': 'TauA', 'flux': '1770', 'alpha': -0.27,
+             'position': 'J2000 05h34m31.94s +22d00m52.2s'},
+            {'label': 'VirA', 'flux': '2400', 'alpha': -0.86,
+             'position': 'J2000 12h30m49.42338s +12d23m28.0439s'}]
+    
+    
+    
+    if includesun:
+        srcs.append({'label': 'Sun', 'flux': str(solar_flux), 'alpha': solar_alpha,
+                     'position': 'SUN'})
+
+    tb.open(visibility)
+    t0 = tb.getcell('TIME', 0)
+    tb.close()
+    # me.set_data_path('/opt/astro/casa-data')
+    ovro = me.observatory('OVRO_MMA')
+    time = me.epoch('UTC', '%fs' % t0)
+    me.doframe(ovro)
+    me.doframe(time)
+
+    msmd.open(visibility)
+    chan_freqs = msmd.chanfreqs(0)  
+    msmd.done()
+    avg_freq=0.5*(chan_freqs[0]+chan_freqs[-1])*1e-6
+    
+    f1=open(filename,'w')
+    num_source=0
+    for s in range(len(srcs) - 1, -1, -1):
+        coord = srcs[s]['position'].split()
+        d0 = None
+        if len(coord) == 1:
+            d0 = me.direction(coord[0])
+            d0_j2000 = me.measure(d0, 'J2000')
+            srcs[s]['position'] = 'J2000 %frad %frad' % (d0_j2000['m0']['value'], d0_j2000['m1']['value'])
+        elif len(coord) == 3:
+            coord[2] = conv_deg(coord[2])
+            d0 = me.direction(coord[0], coord[1], coord[2])
+        else:
+            raise Exception("Unknown direction")
+        d = me.measure(d0, 'AZEL')
+        elev = d['m1']['value']
+        if elev < 0:
+            del srcs[s]
+        else:
+            scale = math.sin(elev) ** 1.6*output_freq/avg_freq
+            print (srcs[s]['label'])
+            print('scale {0:.2f}'.format(scale))
+            write_source_file(f1,srcs[s]['label'],scale,num_source)
+            num_source+=1   
+    
+    return 
 
 
 def flag_ants_from_postcal_autocorr(msfile: str, tavg: bool = False, thresh: float = 8):
@@ -195,7 +331,7 @@ def flag_ants_from_postcal_autocorr(msfile: str, tavg: bool = False, thresh: flo
     else:
         return None
 
-def flag_bad(msfile,flagfile=None,thresh=None):
+def flag_bad_ants(msfile,flagfile=None,thresh=None):
 	if thresh==None:
 		ants=flag_ants_from_postcal_autocorr(msfile)
 	else:
@@ -209,7 +345,7 @@ def flag_bad(msfile,flagfile=None,thresh=None):
 		with open(flagfile,'w') as f:
 			f.write("mode=\'manual\' antenna=\'"+antenna_list+"\'")
 			f.write("\n")
-	files=glob.glob("/home/surajit/ovro-lwa/solar/defaults/*")
+	files=glob.glob("/home/surajit/ovro-lwa-solar/defaults/*")
 	for file1 in files:
 		with open(file1,"r") as f:
 			lines=f.readlines()
@@ -234,7 +370,7 @@ def gen_calibration(visibility, modelcl=None, uvrange='', bcaltb=None):
         print('Model component list does not exist. Generating one from scratch.')
         modelcl = gen_model_cl(visibility)
 
-    # Put the component list to the model column
+    #Put the component list to the model column
     clearcal(visibility, addmodel=True)
     ft(visibility, complist=modelcl, usescratch=True)
     # Now do a bandpass calibration using the model component list
@@ -264,10 +400,10 @@ def apply_calibration(solar_visibility, gaintable=None, doflag=False, do_solar_i
                weighting='uniform', phasecenter=sunpos, niter=500)
         print('Solar image made {0:s}.image'.format(imagename))
 
-def make_fullsky_image(msfile,imagename="full_sky",imsize=4096,cell='2arcmin',minuv=10): ### minuv: minimum uv in lambda
+def make_fullsky_image(msfile,imagename="full_sky",imsize=4096,cell='2arcmin',minuv=10,niter=500): ### minuv: minimum uv in lambda
     os.system("wsclean -no-update-model-required -no-dirty -weight uniform"+\
             " -name "+imagename+" -size "+str(imsize)+" "+str(imsize)+" -scale "+cell+\
-            " -minuv-l "+str(minuv)+" -niter 1000 "+msfile)
+            " -minuv-l "+str(minuv)+" -niter "+str(niter)+" "+msfile)
             
 def get_solar_loc_pix(msfile,image="full_sky"):
     m=get_sun_pos(msfile,str_output=False)
@@ -284,6 +420,7 @@ def get_solar_loc_pix(msfile,image="full_sky"):
 
 def remove_sun_from_model(msfile,image="full_sky",area=100):
     x,y=get_solar_loc_pix(msfile,image)
+    print (x,y)
     head=fits.getheader(image+"-model.fits")
     data=fits.getdata(image+"-model.fits")
     data[0,0,y-area//2:y+area//2,x-area//2:x+area//2]=0.0000
@@ -298,13 +435,7 @@ def remove_all_sources(msfile,imagename='full_sky',imsize=4096,cell='2arcmin',mi
     remove_sun_from_model(msfile,image=imagename)
     outms=msfile[:-3]+"_sun_only.ms"
     predict_model(msfile,outms=outms,image=imagename+"_sun_only")
-    #uvsub(outms)
-    tb.open(outms,nomodify=False)
-    model=tb.getcol("MODEL_DATA")
-    corrected_data=tb.getcol("CORRECTED_DATA")
-    tb.putcol("CORRECTED_DATA",corrected_data-model)
-    tb.flush()
-    tb.close()
+    uvsub(outms)
     return outms
     
 def make_solar_image(msfile,imagename='sun_only',imsize=512,cell='2arcmin',minuv=10):
