7c3d3f9b727edbcdd159805ff12f975b7e1bb79b
Bin Chen <bin.chen@njit.edu>
2023-05-08 15:39:05 -0400

    small fix for autocorr flagging

--- solar_pipeline.py
+++ solar_pipeline.py
@@ -335,7 +335,7 @@ def gen_model_cl(msfile, ref_freq=80.0, output_freq=47.0,
 
 def gen_ant_flags_from_autocorr(msfile, antflagfile=None, datacolumn='DATA', tavg=False,
                                 thresh_core=1.0, thresh_exp=1.0, flag_exp_with_core_stat=True,
-                                flag_either_pol=True,doappend=False, debug=False, doplot=False):
+                                flag_either_pol=False, doappend=False, debug=False, doplot=False):
     """Generates a text file containing the bad antennas.
     DOES NOT ACTUALLY APPLY FLAGS. CURRENTLY SHOULD ONLY BE RUN ON SINGLE SPW MSs.
 
@@ -347,8 +347,12 @@ def gen_ant_flags_from_autocorr(msfile, antflagfile=None, datacolumn='DATA', tav
         :param datacolumn: specify which data column to use. Default to "DATA".
             Could be "CORRECTED_DATA" if the dataset is calibrated
         :param tavg: If set to True, will time average before evaluating flags.
-        :param thresh: Threshold to use for flagging. Default is 4.
         :param antflagfile: Output file that contains the flagged antennas. If not defined, use msfile.replace('.ms', 'antflags')
+        :param thresh_core: Threshold to use for flagging for core antennas. Default is 1.
+        :param thresh_exp: Threshold to use for flagging for expansion antennas. Default is 1.
+        :param flag_exp_with_core_stat: If True, use statistics of core antennas to determine flags for outer antennas
+        :param flag_either_pol: If True, the antenna will be flagged if either polarization is bad (OR scheme).
+                              If False, the antenna will be flagged only if both polarizations are bad (AND scheme).
 
     Returns:
         Path to the text file with the list of antennas to flag (antflagfile).
