8d7d3056329e38f4afd8ad73656b797ea205ffcc
Peijin Zhang <peijin@ustc.edu>
2024-02-19 05:51:43 +0000

    flag baseline

--- ovrolwasolar/calibration.py
+++ ovrolwasolar/calibration.py
@@ -136,7 +136,9 @@ def make_fast_caltb_from_slow(calib_ms, solar_ms, caltb, \
     msmd.done()
     return caltb_fast
     
-def gen_calibration(msfile, modelcl=None, uvrange='', bcaltb=None, logging_level='info', caltable_fold='caltables',refant='202'):
+def gen_calibration(msfile, modelcl=None, uvrange='', bcaltb=None, 
+                    logging_level='info', caltable_fold='caltables',
+                    refant='202', dobaselineflag=False):
     """
     This function is for doing initial self-calibrations using strong sources that are above the horizon
     It is recommended to use a dataset observed at night when the Sun is not in the field of view
@@ -171,6 +173,9 @@ def gen_calibration(msfile, modelcl=None, uvrange='', bcaltb=None, logging_level
     applycal(vis=msfile, gaintable=bcaltb)
     logging.debug("Doing a rflag run on corrected data")
     flagdata(vis=msfile, mode='rflag', datacolumn='corrected')
+    if dobaselineflag:
+        logging.debug("Doing a baseline flagging")
+        flagging.perform_baseline_flagging(msfile, overwrite=True)
     logging.debug("Finding updated and final bandpass table")
     bandpass(msfile, caltable=bcaltb, uvrange=uvrange, combine='scan,field,obs', fillgaps=0,refant=refant)
 
